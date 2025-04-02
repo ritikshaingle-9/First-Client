0b2d4574a115a7217221c5137979cdc774372dac
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-04-02 23:38:04 +0530

    box adjusted

--- src/views/add.jsx
+++ src/views/add.jsx
@@ -36,8 +36,8 @@ function Add() {
   };
 
   return (
-    <div className="bg-slate-200">
-      <h1 className="text-center text-5xl my-4">Add Student</h1>
+    <div className="bg-slate-200  min-h-screen">
+      <h1 className="text-center text-5xl my-4 mb-7">Add student</h1>
 
       <div className="w-1/2 mx-auto shadow-md m-5 p-5 rounded-md border-2 border-gray-200 bg-white">
         <input
